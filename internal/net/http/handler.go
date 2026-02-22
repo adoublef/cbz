31dd39734c0220250bb536a640831b254bc08b4d
Rahim <kristopherab@gmail.com>
2026-02-22 22:04:25 +0000

    handler test ok

---
+++ internal/net/http/handler.go
@@ -0,0 +1,327 @@
+package http
+
+import (
+	"archive/zip"
+	"cmp"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"runtime/trace"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/adoublef/cbz/internal/encoding/html"
+	"golang.org/x/sync/errgroup"
+)
+
+func Handler(apiC, imgC *http.Client, tempDir string) http.Handler {
+	return handleSeries(apiC, imgC, tempDir)
+}
+
+func handleSeries(apiC, imgC *http.Client, tempDir string) handlerFunc {
+	parse := func(w http.ResponseWriter, r *http.Request) (*url.URL, error) {
+		parsed, err := url.Parse(r.URL.Query().Get("series_url"))
+		if err != nil {
+			return nil, httpError(http.StatusBadRequest)
+		}
+		// ensure path is formatted explicitly as /[series]/[id]
+		path := strings.TrimPrefix(parsed.Path, "/")
+		first, rest, more := strings.Cut(path, "/")
+		if !more || first == "" || rest == "" || strings.Contains(rest, "/") {
+			return nil, httpError(http.StatusUnprocessableEntity)
+		}
+		return parsed, nil
+	}
+
+	writeTo := func(zw *zip.Writer, rfs *os.Root, filename string) (err error) {
+		defer rfs.Remove(filename)
+
+		f, err := rfs.Open(filename)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		fi, err1 := f.Stat()
+		zh, err2 := zip.FileInfoHeader(fi)
+		zh.Method = zip.Deflate // needed for the streaming
+		w, err3 := zw.CreateHeader(zh)
+		if err := cmp.Or(err1, err2, err3); err != nil {
+			return err
+		}
+
+		_, err = io.Copy(w, f)
+		return err
+	}
+
+	zipReader := func(ctx context.Context, series *url.URL, rfs *os.Root) io.ReadCloser {
+		g, ctx := errgroup.WithContext(ctx)
+		g.SetLimit(4)
+
+		chapters := make(chan *url.URL)
+		g.Go(func() error {
+			defer close(chapters)
+
+			uri := series.JoinPath("full-chapter-list")
+			req, err1 := http.NewRequestWithContext(ctx, http.MethodGet, uri.String(), nil)
+			res, err2 := apiC.Do(req)
+			if err := cmp.Or(err1, err2); err != nil {
+				return err
+			}
+			defer res.Body.Close()
+
+			if c := res.StatusCode; c != http.StatusOK {
+				return fmt.Errorf("failed query returned %d status code", c)
+			}
+
+			// if there are no chapters, something went wrong
+			// return with an error
+			var count int
+			for url, err := range html.Anchors(res.Body) {
+				if err != nil {
+					return err
+				}
+				// i wanna spawn processes to fetch these pages concurrently
+				// verify that url is correct before sending off
+				//
+				// ensure path is formatted explicitly as /[chapters]/[id]
+				path := strings.TrimPrefix(url.Path, "/")
+				first, rest, more := strings.Cut(path, "/")
+				if !more || first == "" || rest == "" || strings.Contains(rest, "/") {
+					return fmt.Errorf("invalid chapter url: (path: %q)", url.Path)
+				}
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case chapters <- url: // can be assured we have two parts only
+					count++
+				}
+			}
+			if count == 0 {
+				return fmt.Errorf("failed to find any chapters")
+			}
+
+			return nil
+		})
+
+		type msg struct {
+			url   *url.URL
+			reply chan<- string
+			done  func()
+		}
+		images := make(chan msg)
+
+		cbz := make(chan string)
+		g.Go(func() error {
+			defer func() { close(images); close(cbz) }()
+
+			g, ctx := errgroup.WithContext(ctx)
+			g.SetLimit(1)
+
+			for chapter := range chapters {
+				g.Go(func() error {
+					// create a temp directory for the chapter?
+					// spin up two routines
+					g, ctx := errgroup.WithContext(ctx)
+					// 1) handle the initial chapter request
+					var reply = make(chan string) // they all send to this channel
+					var wg sync.WaitGroup         // safe close since i only want to use one channel
+
+					g.Go(func() error {
+						defer func() { wg.Wait(); close(reply) }() // error safe?
+
+						uri := chapter.JoinPath("images")
+						// we need the url Values added here
+						req, err1 := http.NewRequestWithContext(ctx, http.MethodGet, uri.String(), nil)
+						res, err2 := apiC.Do(req)
+						if err := cmp.Or(err1, err2); err != nil {
+							return err
+						}
+						defer res.Body.Close()
+						if c := res.StatusCode; c != http.StatusOK {
+							return fmt.Errorf("failed query returned %d status code", c)
+						}
+
+						var count int
+						for url, err := range html.Images(res.Body) {
+							if err != nil {
+								return err
+							}
+							// ensure the name is valid format /
+							base := path.Base(url.Path)
+							var cn, in int
+							var ext string
+							_, err := fmt.Sscanf(base, "%d-%d.%s", &cn, &in, &ext)
+							if err != nil {
+								return err
+							}
+							// n, err == 3, <nil>
+							select {
+							case <-ctx.Done():
+								return ctx.Err()
+							case images <- msg{url, reply, wg.Done}:
+								wg.Add(1)
+							}
+							count++
+						}
+						if count == 0 {
+							return fmt.Errorf("failed to find any images")
+						}
+						return nil
+					})
+					// 2) gets a response and creates a zip
+					g.Go(func() error {
+						// temp zip file that we can now feed data into
+						f, err := rfs.Create(path.Base(chapter.Path) + ".zip") // for now just use the id, find a way to get the actual position
+						if err != nil {
+							return err
+						}
+						defer f.Close()
+
+						zw := zip.NewWriter(f)
+						defer zw.Flush()
+
+						for filename := range reply {
+							if err := writeTo(zw, rfs, filename); err != nil {
+								return err
+							}
+						}
+						if err := zw.Close(); err != nil {
+							return err
+						}
+						// Todo stream to the client
+						select {
+						case <-ctx.Done():
+							return ctx.Err()
+						case cbz <- path.Base(f.Name()): // the final cbz
+						}
+						return nil
+					})
+
+					return g.Wait()
+				})
+			}
+
+			return g.Wait()
+		})
+
+		g.Go(func() error {
+			g, ctx := errgroup.WithContext(ctx)
+			g.SetLimit(1)
+			for msg := range images {
+				g.Go(func() error {
+					defer msg.done() // no likey
+
+					f, err := rfs.Create(path.Base(msg.url.Path))
+					if err != nil {
+						return err
+					}
+					defer f.Close()
+
+					// query image and download the file
+					req, err1 := http.NewRequestWithContext(ctx, http.MethodGet, msg.url.String(), nil)
+					res, err2 := imgC.Do(req)
+					if err := cmp.Or(err1, err2); err != nil {
+						return err
+					}
+					defer res.Body.Close()
+					if c := res.StatusCode; c != http.StatusOK {
+						return fmt.Errorf("failed query returned %d status code", c)
+					}
+
+					// TODO 1) Content-Type says png, we could verify this
+					// 1) Content-Length for limiting the size when copying
+					size, err := strconv.ParseUint(res.Header.Get("Content-Length"), 10, 64)
+					if err != nil {
+						return err
+					}
+					mbr := http.MaxBytesReader(nil, res.Body, int64(size))
+					n, err := io.Copy(f, mbr)
+					if err != nil {
+						return err
+					} else if n == 0 {
+						return fmt.Errorf("failed to read data")
+					}
+
+					select {
+					case <-ctx.Done():
+						return ctx.Err()
+					case msg.reply <- path.Base(f.Name()): // signal that we are done
+					}
+					return nil
+				})
+			}
+			return g.Wait()
+		})
+
+		pr, pw := io.Pipe()
+		g.Go(func() error {
+			// collect all previous chapters
+			zw := zip.NewWriter(pw)
+			defer zw.Flush()
+			for filename := range cbz {
+				if err := writeTo(zw, rfs, filename); err != nil {
+					return err
+				}
+			}
+			return zw.Close()
+		})
+		go func() { pw.CloseWithError(g.Wait()) }()
+		return pr
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) error {
+		ctx, task := trace.NewTask(r.Context(), "handleSeries")
+		defer task.End()
+
+		series, err := parse(w, r)
+		if err != nil {
+			return err
+		}
+
+		// make a temp directory for the series
+		tempDir, err := os.MkdirTemp(tempDir, "")
+		if err != nil {
+			return err
+		}
+		defer os.RemoveAll(tempDir)
+
+		rfs, err := os.OpenRoot(tempDir)
+		if err != nil {
+			return err
+		}
+		defer rfs.Close()
+
+		zr := zipReader(ctx, series, rfs)
+		defer zr.Close()
+
+		_, err = io.Copy(w, zr)
+		return err
+	}
+}
+
+type handlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+func (h handlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h(w, r)
+	if err == nil {
+		return
+	}
+
+	if err, ok := errors.AsType[httpError](err); ok {
+		http.Error(w, err.Error(), int(err))
+		return
+	}
+	log.Printf("unexpected error occured: %v\n", err)
+}
+
+type httpError int
+
+func (e httpError) Error() string { return http.StatusText(int(e)) }
---
+++ internal/net/http/handler_test.go
@@ -0,0 +1,200 @@
+package http_test
+
+import (
+	"cmp"
+	"embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"strings"
+	"testing"
+
+	. "github.com/adoublef/cbz/internal/net/http"
+	"github.com/zhyee/zipstream"
+)
+
+var numChapters, numImages int
+
+func init() {
+	flag.IntVar(&numChapters, "chapters", 1, "number of chapters")
+	flag.IntVar(&numImages, "images", 1, "number of images")
+}
+
+func TestHandler(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		ctx := t.Context()
+
+		imgC, imgURL := imgClient(t)
+		apiC, apiURL := apiClient(t, imgURL, numChapters, numImages)
+
+		tempDir := t.TempDir()
+		testC, testURL := testClient(t, apiC, imgC, tempDir)
+
+		url := fmt.Sprintf("%s/?series_url=%s", testURL, apiURL+"/series/1")
+		req, err1 := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		res, err2 := testC.Do(req)
+		ok(t, cmp.Or(err1, err2))
+		equal(t, res.StatusCode, http.StatusOK) // stream means this is always going to be the case
+
+		// stream zip
+		zr := zipstream.NewReader(res.Body)
+		for zr.Next() {
+			_, err := zr.Entry()
+			ok(t, err)
+		}
+		ok(t, zr.Err())
+
+		// f, err := os.Create("output.zip")
+		// ok(t, err)
+
+		// _, err = io.Copy(f, res.Body)
+		// ok(t, err)
+		// ok(t, f.Close())
+
+		ok(t, res.Body.Close())
+	})
+}
+
+func testClient(t testing.TB, apiC, imgC *http.Client, tempDir string) (*http.Client, string) {
+	t.Helper()
+
+	s := httptest.NewServer(Handler(apiC, imgC, tempDir))
+	t.Cleanup(s.Close)
+
+	return s.Client(), s.URL
+}
+
+func ok(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("%s: unexpected error: %v", t.Name(), err)
+	}
+}
+
+func equal[K comparable](t testing.TB, got, want K) {
+	t.Helper()
+	if got != want {
+		t.Errorf("%s: got=%v; want=%v", t.Name(), got, want)
+	}
+}
+
+//go:embed testdata/*.html testdata/*.xml testdata/*.jpg
+var embedFS embed.FS
+
+func apiClient(t testing.TB, imgURL string, chapters, images int) (apiC *http.Client, apiURL string) {
+	t.Helper()
+
+	funcMap := template.FuncMap{
+		// See https://stackoverflow.com/a/22716709
+		"N":    func(n int) []struct{} { return make([]struct{}, n) },
+		"sub":  func(a, b int) int { return a - b },
+		"inc":  func(i int) int { return i + 1 },
+		"iota": func(i int) string { return strconv.Itoa(i) },
+		"join": func(sep string, s ...string) string { return strings.Join(s, sep) },
+		"url": func(base string, s ...string) string {
+			u, err := url.JoinPath(base, s...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return u
+		},
+	}
+
+	series, err1 := template.New("series.html").Funcs(funcMap).ParseFS(embedFS, "testdata/series.html")
+	rss, err2 := template.New("rss.xml").Funcs(funcMap).ParseFS(embedFS, "testdata/rss.xml")
+	chapter, err3 := template.New("chapter.html").Funcs(funcMap).ParseFS(embedFS, "testdata/chapter.html")
+	if err := cmp.Or(err1, err2, err3); err != nil {
+		t.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	// return series
+	mux.HandleFunc("GET /series/{series}/full-chapter-list", func(w http.ResponseWriter, r *http.Request) {
+		// some formatted string
+		_, err := strconv.ParseUint(r.PathValue("series"), 10, 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		data := struct {
+			N       int
+			BaseURL string
+		}{
+			N:       chapters,
+			BaseURL: apiURL,
+		}
+		if err := series.Execute(w, data); err != nil {
+			t.Fatal(err)
+		}
+	})
+	// return series (rss)
+	mux.HandleFunc("GET /series/{series}/rss", func(w http.ResponseWriter, r *http.Request) {
+		// some formatted string
+		_, err := strconv.ParseUint(r.PathValue("series"), 10, 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := rss.Execute(w, nil); err != nil {
+			t.Fatal(err)
+		}
+	})
+	// return chapters
+	mux.HandleFunc("GET /chapters/{chapter}/images", func(w http.ResponseWriter, r *http.Request) {
+		// some formatted string
+		id, err := strconv.ParseUint(r.PathValue("chapter"), 10, 64)
+		if err != nil {
+			t.Fatal(err)
+		}
+		data := struct {
+			N       int
+			Chapter int
+			BaseURL string
+		}{
+			N:       images,
+			Chapter: int(id),
+			BaseURL: imgURL,
+		}
+		if err := chapter.Execute(w, data); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	s := httptest.NewServer(mux)
+	t.Cleanup(s.Close)
+	return s.Client(), s.URL
+}
+
+func imgClient(t testing.TB) (imgC *http.Client, imgURL string) {
+	t.Helper()
+
+	mux := http.NewServeMux()
+	// return a file
+	mux.HandleFunc("GET /{image}", func(w http.ResponseWriter, r *http.Request) {
+		// parse path?
+
+		f, err1 := embedFS.Open("testdata/image.jpg") // base64
+		fi, err2 := f.Stat()
+		if err := cmp.Or(err1, err2); err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+
+		// Content-Length
+		w.Header().Set("Content-Length", strconv.Itoa(int(fi.Size())))
+		// Content-Type ?
+
+		if r.Method != http.MethodHead {
+			if _, err := io.Copy(w, f); err != nil {
+				t.Fatal(err)
+			}
+		}
+	})
+
+	s := httptest.NewServer(mux)
+	t.Cleanup(s.Close)
+	return s.Client(), s.URL
+}
